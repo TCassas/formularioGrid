6ece838adb58cf65034507f0c1c7a0a1241e49e6
Tomás Cassas <tomascassas@gmail.com>
2019-11-09 05:01:47 -0300

    Control visual de que es lo que tiene que ingresar en cada input.

--- scriptFormulario.js
+++ scriptFormulario.js
@@ -8,6 +8,8 @@ var input2 = document.getElementById('input2');
 var input3 = document.getElementById('input3');
 var input4 = document.getElementById('input4');
 
+var verificadorTexto = document.getElementsByClassName("cantidadLetras");
+
 
 // var verificacionImagen = document.querySelectorAll(".imagen");
 var boton = document.getElementById('botonEnviar');
@@ -15,7 +17,7 @@ var boton = document.getElementById('botonEnviar');
 input1.addEventListener('focus', (e) => {
   if(input1.value === "") {
     label1.classList.toggle("inputSeleccionado");
-    formularioCompleto();
+    verificadorTexto[0].style.left = "-80px";
     //input1.style.width = "98%";
   }
 });
@@ -23,23 +25,28 @@ input1.addEventListener('focus', (e) => {
 input1.addEventListener('blur', (e) => {
   if(input1.value === "") {
     label1.classList.toggle("inputSeleccionado");
+    verificadorTexto[0].style.opacity = "4px solid red";
     //input1.style.width = "107.8%";
   }
 
   if(input1.value.length > 0) {
     input1.classList.add("inputValido");
     input1.classList.remove("inputInvalido");
+    verificadorTexto[0].style.borderLeft = "4px solid hsl(154, 59%, 51%)";
+    verificadorTexto[0].style.left = "-80px";
   }
 
-  if(input1.value.length === 0) {
+  if(input1.value.length === 0 || input1.value.length > 50) {
     input1.classList.add("inputValido");
     input1.classList.add("inputInvalido");
+    verificadorTexto[0].style.borderLeft = "4px solid red";
   }
 });
 
 input2.addEventListener('focus', (e) => {
   if(input2.value === "") {
     label2.classList.toggle("inputSeleccionado");
+    verificadorTexto[1].style.left = "-80px";
     //input2.style.width = "98%";
   }
 });
@@ -53,17 +60,21 @@ input2.addEventListener('blur', (e) => {
   if(input2.value.length > 0) {
     input2.classList.add("inputValido");
     input2.classList.remove("inputInvalido");
+    verificadorTexto[1].style.borderLeft = "4px solid hsl(154, 59%, 51%)";
+    verificadorTexto[1].style.left = "-80px";
   }
 
-  if(input2.value.length === 0) {
+  if(input2.value.length === 0 || input2.value.length > 50) {
     input2.classList.remove("inputValido");
     input2.classList.add("inputInvalido");
+    verificadorTexto[1].style.borderLeft = "4px solid red";
   }
 });
 
 input3.addEventListener('focus', (e) => {
   if(input3.value === "") {
     label3.classList.toggle("inputSeleccionado");
+    verificadorTexto[2].style.left = "-80px";
     //input3.style.width = "98%";
   }
 });
@@ -77,17 +88,21 @@ input3.addEventListener('blur', (e) => {
   if(input3.value.length > 0) {
     input3.classList.add("inputValido");
     input3.classList.remove("inputInvalido");
+    verificadorTexto[2].style.borderLeft = "4px solid hsl(154, 59%, 51%)";
+    verificadorTexto[2].style.left = "-80px";
   }
 
-  if(input3.value.length === 0) {
+  if(input3.value.length === 0 || input3.value.length > 50) {
     input3.classList.toggle("inputValido");
     input3.classList.add("inputInvalido");
+    verificadorTexto[2].style.borderLeft = "4px solid red";
   }
 });
 
 input4.addEventListener('focus', (e) => {
   if(input4.value === "") {
     label4.classList.toggle("inputSeleccionado");
+    verificadorTexto[3].style.left = "-80px";
     //input4.style.width = "98%";
   }
 });
@@ -101,11 +116,14 @@ input4.addEventListener('blur', (e) => {
   if(input4.value.length > 0) {
     input4.classList.add("inputValido");
     input4.classList.remove("inputInvalido");
+    verificadorTexto[3].style.borderLeft = "4px solid hsl(154, 59%, 51%)";
+    verificadorTexto[3].style.left = "-80px";
   }
 
-  if(input4.value.length === 0) {
+  if(input4.value.length === 0 || input4.value.length > 50) {
     input4.classList.toggle("inputValido");
     input4.classList.add("inputInvalido");
+    verificadorTexto[3].style.borderLeft = "4px solid red";
   }
 });
 
