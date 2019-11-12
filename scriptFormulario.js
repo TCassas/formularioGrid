48586303389c500327f16148241367846a27cb2f
Tomás Cassas <tomascassas@gmail.com>
2019-11-12 00:26:32 -0300

    Corrijo los id de los input para que encajen con los atributos for de los labels.

--- scriptFormulario.js
+++ scriptFormulario.js
@@ -3,10 +3,10 @@ var label2 = document.getElementById('label2');
 var label3 = document.getElementById('label3');
 var label4 = document.getElementById('label4');
 
-var input1 = document.getElementById('input1');
-var input2 = document.getElementById('input2');
-var input3 = document.getElementById('input3');
-var input4 = document.getElementById('input4');
+var input1 = document.getElementById('nombre');
+var input2 = document.getElementById('apellido');
+var input3 = document.getElementById('email');
+var input4 = document.getElementById('contrasenia');
 
 var verificadorTexto = document.getElementsByClassName("cantidadLetras");
 
